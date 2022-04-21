82e8dd91fa406616ecf740551a4317a9e8b50f74
BastingSumo <69335910+BastingSumo@users.noreply.github.com>
2022-04-21 15:53:31 +0800

    final changes before hosting

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,8 @@
-function NavBar({className}) { 
-    return <div className ={className}>
-    <button className = "button"> Home </button>
-    <button className = "button"> About Me </button>
-    <button className = "button"> MyProjects  </button>
+function NavBar({ className }) {
+    return <div className={className}>
+        <button className="button" disabled={true}> Home </button>
+        <button className="button" disabled={true}> About Me </button>
+        <button className="button" disabled={true}> MyProjects  </button>
     </div>
 }
 
